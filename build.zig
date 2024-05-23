0dee348ad6be964a35efb9ed015969f6a1e50654
Zoee Silcock <mrzoee@gmail.com>
2024-05-23 16:19:04 +0200

    Make the output name match the directory name

--- build.zig
+++ build.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const exe = b.addExecutable(.{
-        .name = "handmade",
+        .name = "handmade-zig",
         .root_source_file = b.path("src/main.zig"),
         .target = b.host,
     });
