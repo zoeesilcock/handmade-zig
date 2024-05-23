c797ef76af8624a87d09fc1a5647490a5898fd32
Zoee Silcock <mrzoee@gmail.com>
2024-05-23 08:20:16 +0200

    Add an example test

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,11 @@
 const std = @import("std");
+const expect = std.testing.expect;
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
     try stdout.print("Hello, {s}!\n", .{"World"});
+}
+
+test "sanity" {
+    try expect(true);
 }
