16922573e5a78b9cf3c0dcd1cea37ca9b4e3e27b
Zoee Silcock <mrzoee@gmail.com>
2026-01-22 06:33:14 +0100

    Decoding PNG length and distance extra bits, day 456

--- src/png.zig
+++ src/png.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const shared = @import("shared.zig");
 
 const PNG_HUFFMAN_MAX_BIT_COUNT = 16;
+const MAX_REVERSE_BITS_COUNT = 13;
 
 const Signature: [8]u8 = .{ 137, 80, 78, 71, 13, 10, 26, 10 };
 const Header = extern struct {
@@ -78,18 +79,8 @@ const Huffman = struct {
 
                 var entry_index: u32 = 0;
                 while (entry_index < entry_count) : (entry_index += 1) {
-                    const base_index: u32 = (entry_index << @as(u5, @intCast(code_length_in_bits))) | code;
-                    var index: u32 = 0;
-
-                    bit_index = 0;
-
-                    // TODO: We can actually just construct this bit pattern backwards, eventually. If this turns
-                    // out to be what they actually meant, which isn't clear.
-                    while (bit_index <= (self.max_code_length_in_bits / 2)) : (bit_index += 1) {
-                        const inverse: u5 = @intCast(self.max_code_length_in_bits - (bit_index + 1));
-                        index |= ((base_index >> @as(u5, @intCast(bit_index))) & 0x1) << inverse;
-                        index |= ((base_index >> inverse) & 0x1) << @as(u5, @intCast(bit_index));
-                    }
+                    const base_index: u32 = (code << @as(u5, @intCast(arbitrary_bits))) | entry_index;
+                    const index: u32 = reverseBits(base_index, self.max_code_length_in_bits);
 
                     var entry: *HuffmanEntry = &self.entries[index];
 
@@ -232,10 +223,32 @@ fn endianSwap(value: *align(1) u32) void {
     }
 }
 
+fn endianSwapU16(value: *align(1) u16) void {
+    if (false) {
+        const v: u16 = value.*;
+        value.* = ((v << 8) | (v >> 8));
+    } else {
+        value.* = @byteSwap(value.*);
+    }
+}
+
 test "endianSwap" {
     var value: u32 = 0x44332211;
     endianSwap(&value);
     try std.testing.expectEqual(0x11223344, value);
+}
+
+fn reverseBits(value: u32, bit_count: u32) u32 {
+    var result: u32 = 0;
+
+    var bit_index: u32 = 0;
+    while (bit_index <= (bit_count / 2)) : (bit_index += 1) {
+        const inverse: u5 = @intCast(bit_count - (bit_index + 1));
+        result |= ((value >> @as(u5, @intCast(bit_index))) & 0x1) << inverse;
+        result |= ((value >> inverse) & 0x1) << @as(u5, @intCast(bit_index));
+    }
+
+    return result;
 }
 
 fn fourcc(string: []const u8) u32 {
@@ -247,8 +260,15 @@ fn fourcc(string: []const u8) u32 {
         @as(u32, @intCast(string[3])) << 24;
 }
 
-fn allocatePixels(allocator: std.mem.Allocator, width: u32, height: u32, bytes_per_pixel: u32) []u8 {
-    return allocator.alloc(u8, width * height * bytes_per_pixel) catch unreachable;
+fn allocatePixels(
+    allocator: std.mem.Allocator,
+    width: u32,
+    height: u32,
+    bytes_per_pixel: u32,
+    opt_extra_bytes: ?u32,
+) []u8 {
+    const extra_bytes: u32 = opt_extra_bytes orelse 0;
+    return allocator.alloc(u8, width * height * bytes_per_pixel + (extra_bytes * height)) catch unreachable;
 }
 
 fn allocateChunk(allocator: std.mem.Allocator) *StreamingChunk {
@@ -265,10 +285,82 @@ fn allocateHuffman(allocator: std.mem.Allocator, max_code_length_in_bits: u32) H
     return result;
 }
 
+const length_extra = [_]HuffmanEntry{
+    .{ .symbol = 3, .bits_used = 0 }, // 257
+    .{ .symbol = 4, .bits_used = 0 }, // 258
+    .{ .symbol = 5, .bits_used = 0 }, // 259
+    .{ .symbol = 6, .bits_used = 0 }, // 260
+    .{ .symbol = 7, .bits_used = 0 }, // 261
+    .{ .symbol = 8, .bits_used = 0 }, // 262
+    .{ .symbol = 9, .bits_used = 0 }, // 263
+    .{ .symbol = 10, .bits_used = 0 }, // 264
+    .{ .symbol = 11, .bits_used = 1 }, // 265
+    .{ .symbol = 13, .bits_used = 1 }, // 266
+    .{ .symbol = 15, .bits_used = 1 }, // 267
+    .{ .symbol = 17, .bits_used = 1 }, // 268
+    .{ .symbol = 19, .bits_used = 2 }, // 269
+    .{ .symbol = 23, .bits_used = 2 }, // 270
+    .{ .symbol = 27, .bits_used = 2 }, // 271
+    .{ .symbol = 31, .bits_used = 2 }, // 272
+    .{ .symbol = 35, .bits_used = 3 }, // 273
+    .{ .symbol = 43, .bits_used = 3 }, // 274
+    .{ .symbol = 51, .bits_used = 3 }, // 275
+    .{ .symbol = 59, .bits_used = 3 }, // 276
+    .{ .symbol = 67, .bits_used = 4 }, // 277
+    .{ .symbol = 83, .bits_used = 4 }, // 278
+    .{ .symbol = 99, .bits_used = 4 }, // 279
+    .{ .symbol = 115, .bits_used = 4 }, // 280
+    .{ .symbol = 131, .bits_used = 5 }, // 281
+    .{ .symbol = 163, .bits_used = 5 }, // 282
+    .{ .symbol = 195, .bits_used = 5 }, // 283
+    .{ .symbol = 227, .bits_used = 5 }, // 284
+    .{ .symbol = 258, .bits_used = 0 }, // 285
+};
+
+const distance_extra = [_]HuffmanEntry{
+    .{ .symbol = 1, .bits_used = 0 }, // 0
+    .{ .symbol = 2, .bits_used = 0 }, // 1
+    .{ .symbol = 3, .bits_used = 0 }, // 2
+    .{ .symbol = 4, .bits_used = 0 }, // 3
+    .{ .symbol = 5, .bits_used = 1 }, // 4
+    .{ .symbol = 7, .bits_used = 1 }, // 5
+    .{ .symbol = 9, .bits_used = 2 }, // 6
+    .{ .symbol = 13, .bits_used = 2 }, // 7
+    .{ .symbol = 17, .bits_used = 3 }, // 8
+    .{ .symbol = 25, .bits_used = 3 }, // 9
+    .{ .symbol = 33, .bits_used = 4 }, // 10
+    .{ .symbol = 49, .bits_used = 4 }, // 11
+    .{ .symbol = 65, .bits_used = 5 }, // 12
+    .{ .symbol = 97, .bits_used = 5 }, // 13
+    .{ .symbol = 129, .bits_used = 6 }, // 14
+    .{ .symbol = 193, .bits_used = 6 }, // 15
+    .{ .symbol = 257, .bits_used = 7 }, // 16
+    .{ .symbol = 385, .bits_used = 7 }, // 17
+    .{ .symbol = 513, .bits_used = 8 }, // 18
+    .{ .symbol = 769, .bits_used = 8 }, // 19
+    .{ .symbol = 1025, .bits_used = 9 }, // 20
+    .{ .symbol = 1573, .bits_used = 9 }, // 21
+    .{ .symbol = 2049, .bits_used = 10 }, // 22
+    .{ .symbol = 3073, .bits_used = 10 }, // 23
+    .{ .symbol = 4097, .bits_used = 11 }, // 24
+    .{ .symbol = 6145, .bits_used = 11 }, // 25
+    .{ .symbol = 8193, .bits_used = 12 }, // 26
+    .{ .symbol = 12289, .bits_used = 12 }, // 27
+    .{ .symbol = 16385, .bits_used = 13 }, // 28
+    .{ .symbol = 24577, .bits_used = 13 }, // 29
+};
+
+var reverse_bits: [1 << MAX_REVERSE_BITS_COUNT]u16 = undefined;
+
 /// This is not meant to be fault tolerant. It only loads specifically what we expect, and is happy to crash otherwise.
 fn parsePNG(file: StreamingBuffer, allocator: std.mem.Allocator) void {
     var supported: bool = false;
     var at: StreamingBuffer = file;
+
+    var r_index: u16 = 0;
+    while (r_index < reverse_bits.len) : (r_index += 1) {
+        reverse_bits[r_index] = @intCast(reverseBits(r_index, MAX_REVERSE_BITS_COUNT));
+    }
 
     if (at.consumeType(Header)) |header| {
         _ = header;
@@ -355,8 +447,8 @@ fn parsePNG(file: StreamingBuffer, allocator: std.mem.Allocator) void {
                 if (supported) {
                     std.log.info("Decompressing...", .{});
 
-                    const decompressed_pixels: []u8 = allocatePixels(allocator, width, height, 4);
-                    _ = decompressed_pixels;
+                    const decompressed_pixels: []u8 = allocatePixels(allocator, width, height, 4, 1);
+                    var dest = decompressed_pixels;
 
                     var bfinal: u32 = 0;
                     while (bfinal == 0) {
@@ -365,10 +457,20 @@ fn parsePNG(file: StreamingBuffer, allocator: std.mem.Allocator) void {
 
                         if (btype == 0) {
                             compressed_data.flushByte();
-                            const len: u32 = compressed_data.consumeBits(16);
-                            const nlen: i32 = @bitCast(compressed_data.consumeBits(16));
-                            if (len != -nlen) {
+                            var len: u16 = compressed_data.consumeType(u16).?.*;
+                            const nlen: u16 = compressed_data.consumeType(u16).?.*;
+                            if (len != -@as(i32, @intCast(nlen))) {
                                 std.log.err("LEN/NLEN mismatch.", .{});
+                            }
+
+                            // TODO: Is this actually supposed to be swapped?
+                            endianSwapU16(&len);
+
+                            var source: [*]u8 = compressed_data.consumeSize(len).?;
+                            while (len > 0) : (len -= 1) {
+                                dest[0] = source[0];
+                                dest.ptr += 1;
+                                source += 1;
                             }
                         } else if (btype == 3) {
                             std.log.err("BTYPE of {d} encountered.", .{btype});
@@ -395,21 +497,19 @@ fn parsePNG(file: StreamingBuffer, allocator: std.mem.Allocator) void {
                                     hclen_table[hclen_swizzle[index]] = compressed_data.consumeBits(3);
                                 }
 
-                                var dictionary_huffman: Huffman = allocateHuffman(allocator, 8);
-                                dictionary_huffman.compute(hclen, &hclen_table, null);
+                                var dictionary_huffman: Huffman = allocateHuffman(allocator, 7);
+                                dictionary_huffman.compute(hclen_swizzle.len, &hclen_table, null);
 
-                                var literal_length_distance_table: [512]u32 = [1]u32{0} ** 512;
+                                var literal_length_distance_table: [512]u32 = undefined;
                                 var literal_length_count: u32 = 0;
                                 const length_count: u32 = hlit + hdist;
-                                index = 0;
                                 while (literal_length_count < length_count) {
                                     var repeat_count: u32 = 1;
                                     var repeat_value: u32 = 0;
                                     const encoded_length: u32 = dictionary_huffman.decode(&compressed_data);
 
                                     if (encoded_length <= 15) {
-                                        literal_length_distance_table[literal_length_count] = encoded_length;
-                                        literal_length_count += 1;
+                                        repeat_value = encoded_length;
                                     } else if (encoded_length == 16) {
                                         repeat_count = 3 + compressed_data.consumeBits(2);
                                         std.debug.assert(literal_length_count > 0);
@@ -438,26 +538,42 @@ fn parsePNG(file: StreamingBuffer, allocator: std.mem.Allocator) void {
 
                             while (true) {
                                 const literal_length: u32 = literal_length_huffman.decode(&compressed_data);
-                                if (literal_length < 256) {
-                                    const out: u32 = literal_length;
-                                    _ = out;
-                                    // TODO: Write here.
-                                } else if (literal_length > 256) {
-                                    const length: u32 = literal_length - 256;
-                                    const distance: u32 = distance_huffman.decode(&compressed_data);
-                                    _ = distance;
-                                    var index: u32 = 0;
-                                    while (index < length) : (index += 1) {
-                                        // TODO: Write here
+                                if (literal_length <= 255) {
+                                    const out: u32 = literal_length & 0xff;
+                                    dest[0] = @truncate(out);
+                                } else if (literal_length >= 257) {
+                                    const length_table_index: u32 = literal_length - 257;
+                                    const length_table_entry: HuffmanEntry = length_extra[length_table_index];
+                                    var length: u32 = length_table_entry.symbol;
+                                    if (length_table_entry.bits_used > 0) {
+                                        const extra_bits: u32 =
+                                            compressed_data.consumeBits(length_table_entry.bits_used);
+                                        // extra_bits = reverseBits(extra_bits, length_table_entry.bits_used);
+                                        length += extra_bits;
+                                    }
+
+                                    const distance_table_index: u32 = distance_huffman.decode(&compressed_data);
+                                    const distance_table_entry: HuffmanEntry = distance_extra[distance_table_index];
+                                    var distance: u32 = distance_table_entry.symbol;
+                                    if (distance_table_entry.bits_used > 0) {
+                                        const extra_bits: u32 =
+                                            compressed_data.consumeBits(distance_table_entry.bits_used);
+                                        // extra_bits = reverseBits(extra_bits, distance_table_entry.bits_used);
+                                        distance += extra_bits;
+                                    }
+
+                                    var source: [*]u8 = @ptrFromInt(@intFromPtr(dest.ptr) - distance);
+                                    std.debug.assert(@intFromPtr(source) >= @intFromPtr(decompressed_pixels.ptr));
+
+                                    while (length > 0) : (length -= 1) {
+                                        dest[0] = source[0];
+                                        dest.ptr += 1;
+                                        source += 1;
                                     }
                                 } else {
                                     break;
                                 }
                             }
-
-                            // TODO: REMOVE THIS!
-                            bfinal = 1;
-                            break;
                         }
                     }
                 }
